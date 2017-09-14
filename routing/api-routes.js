c4e9bce15804eb0504f3f71b0b7724f64f0cf21b
mariomelchor <mario@mariomelchor.com>
2017-09-13 17:43:23 -0700

    Added body-parser package, Added process.env.PORT, Used module.exports for friends.js, Added action url for form in survey,

--- routing/api-routes.js
+++ routing/api-routes.js
@@ -1,8 +1,23 @@
-var path = require('path');
+var friends = require('../app/data/friends.js');
 
 module.exports = function(app){
-  // Show Friend JSON
+  // Show Friends JSON
   app.get('/api/friends', function (req, res) {
-    res.json({ user: 'tobi' });
+    res.json(friends);
+  })
+
+  // Take in Post request
+  app.post('/api/friends', function (req, res) {
+    res.send('Got a POST request');
+    console.log(res.body);
+
+    var newFriend = req.body;
+    // newFriend.routeName = newFriend.name.replace(/\s+/g, "").toLowerCase();
+  
+    console.log(newFriend);
+    characters.push(newFriend);
+  
+    res.json(newFriend);
+
   })
 }
--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 app.use(express.static('./public'));
 
 require('./routing/api-routes.js')(app);
 require('./routing/html-routes.js')(app);
 
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
 // Server on Port 3000
-app.listen(3000, function () {
+app.listen(PORT, function () {
   console.log('Friend Finder App listening on port 3000!');
 })
 
