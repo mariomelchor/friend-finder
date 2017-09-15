71167d3099299c40d1b97033652f06017072f9c8
mariomelchor <mario@mariomelchor.com>
2017-09-15 15:38:30 -0700

    Updated html in home, Added new classes, Removed fs

--- routing/api-routes.js
+++ routing/api-routes.js
@@ -1,4 +1,3 @@
-var fs = require('fs');
 var friends = require('../app/data/friends.js');
 
 module.exports = function(app){
@@ -12,14 +11,6 @@ module.exports = function(app){
     // Get post request and add to friends array
     var newFriend = req.body;
     friends.push(newFriend);
-
-    // // Update freinds.js file with newFriend
-    // fs.writeFile('app/data/friends.js', JSON.stringify( friends, null, 2 ), function(error) {
-    //   // if there is an error, log the error
-    //   if (error) {
-    //     console.log(error);
-    //   }
-    // });
 
     // Respond with newFriend
     res.json(friends);
