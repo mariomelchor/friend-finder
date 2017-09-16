ed5125340d013c8e54a74f0a7e7aa26fa7b4b7c0
mariomelchor <mario@mariomelchor.com>
2017-09-15 20:10:17 -0700

    Updated friends.js, Match appending, Updates to suvey.html

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 var friends = [
   {
-    name:"Ahmed",
-    photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    name:"Joseph",
+    photo:"https://cdn.pixabay.com/photo/2015/12/09/22/26/people-1085695_960_720.png",
     scores:[
       5,
       1,
--- routing/api-routes.js
+++ routing/api-routes.js
@@ -10,10 +10,39 @@ module.exports = function(app){
   app.post('/api/friends', function (req, res) {
     // Get post request and add to friends array
     var newFriend = req.body;
+
+    // Converts all scores to int
+    for( var i=0; i < newFriend.scores.length; i++ ) { 
+      newFriend.scores[i] = +newFriend.scores[i]; 
+    } 
+
+    // loop through friends array
+    for( var i=0; i < friends.length; i++ ) {
+
+      var sum = 100;
+      var num  = 0;
+      var match = {};
+
+      // If array lenghts match
+      if ( newFriend.scores.length == friends[i].scores.length ) {
+
+        // loop through scores and get differences
+        for ( var j = 0; j < newFriend.scores.length; j++ ) {
+          num += Math.abs( newFriend.scores[j] - friends[i].scores[j] );
+          // Whos a match
+          if ( num <= sum ) {
+            sum = num;
+            match = friends[i];
+          }
+        }
+      
+      }
+    }
+    
+    // Adds to friends array
     friends.push(newFriend);
 
-    // Respond with newFriend
-    res.json(friends);
-
+    // Respond with 
+    res.json(match);
   })
 }
